8859f0ab470e1051d76db4eb30e63aa58d81f5ce
Baur Zhan <102077363+baurzhaan@users.noreply.github.com>
2023-05-21 18:38:14 +0100

    Fill the hash by the vowels only

--- lesson2/task4_hash_alphabet.rb
+++ lesson2/task4_hash_alphabet.rb
@@ -1,6 +1,7 @@
 # Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
 
 alphabet = {}
-('a'..'z').to_a.each_with_index { |element, index| alphabet[element.to_sym] = index + 1 }
+
+('a'..'z').to_a.each_with_index { |element, index| alphabet[element.to_sym] = index + 1 if element =~ /[aeiou]/ }
 
 puts alphabet
