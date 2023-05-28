6cf4b6feeb70832e952a87b5d15b184afe046178
Baur Zhan <102077363+baurzhaan@users.noreply.github.com>
2023-05-28 18:43:21 +0100

    Имеет номер (произвольная строка) и тип (грузовой, пассажирский) и количество вагонов, эти данные указываются при создании экземпляра класса. Может набирать скорость. Может возвращать текущую скорость. Может тормозить (сбрасывать скорость до нуля). Может возвращать количество вагонов. Может прицеплять/отцеплять вагоны.

--- lesson3-train-station/Station.rb
+++ lesson3-train-station/Station.rb
@@ -17,7 +17,60 @@ class Station
   end
 end
 
+class Train
+  attr_accessor :train_id, :train_type, :train_wagon_count, :speed
+
+  def initialize(train_id, train_type, train_wagon_count)
+    self.train_id = train_id
+    self.train_type = train_type
+    self.train_wagon_count = train_wagon_count
+    self.speed = 0
+  end
+
+  def accelerate(value)
+    self.speed += value
+  end
+
+  def stop
+    self.speed = 0
+  end
+
+  def add_wagon
+    self.train_wagon_count += 1 if self.speed == 0
+  end
+
+  def remove_wagon
+    self.train_wagon_count -= 1 if self.speed == 0
+  end
+end
+
 # main
+
+train1 = Train.new("train1", "passenger", 10)
+p train1
+puts "#{train1.train_id} это поезд типа #{train1.train_type} с #{train1.train_wagon_count} вагонами"
+puts "Скорость поезда - #{train1.speed}"
+puts "Количество вагонов - #{train1.train_wagon_count}"
+train1.add_wagon
+puts "Количество вагонов (попытка добавить вагон на скорости 0) - #{train1.train_wagon_count}"
+
+train1.accelerate(10)
+puts "Скорость поезда - #{train1.speed}"
+puts "Количество вагонов - #{train1.train_wagon_count}"
+train1.add_wagon
+puts "Количество вагонов (попытка добавить вагон на скорости) - #{train1.train_wagon_count}"
+
+train1.stop
+puts "Скорость поезда - #{train1.speed}"
+puts "Количество вагонов - #{train1.train_wagon_count}"
+train1.add_wagon
+puts "Количество вагонов (попытка добавить вагон после остановки) - #{train1.train_wagon_count}"
+puts
+
+train2 = Train.new("train2", "cargo", 20)
+p train2
+puts "#{train2.train_id} это поезд типа #{train2.train_type} с #{train2.train_wagon_count} вагонами"
+puts
 
 station1 = Station.new("Station1")
 p station1
@@ -26,10 +79,11 @@ station1.accept_train("train2")
 station1.accept_train("train3")
 puts "Количество поездов на станции #{station1.name}: #{station1.trains_on_station}"
 puts "Список поездов на станции #{station1.name}: #{station1.trains_list}"
-
 puts
+
 station2 = Station.new("Station2")
 p station2
 station2.accept_train("train1")
 puts "Количество поездов на станции #{station2.name}: #{station2.trains_on_station}"
 puts "Список поездов на станции #{station2.name}: #{station2.trains_list}"
+puts
