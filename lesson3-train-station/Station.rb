d95b8e6fe782f974c701f748194ceede097ed5cb
Baur Zhan <102077363+baurzhaan@users.noreply.github.com>
2023-05-29 22:46:39 +0100

    Tasks for the Station class.

--- lesson3-train-station/Station.rb
+++ lesson3-train-station/Station.rb
@@ -1,13 +1,28 @@
-class Station
-  attr_accessor :name, :trains_list
+require_relative "Train"
 
-  def initialize(name)
+class Station
+  attr_accessor :name
+  attr_reader :trains
+
+  def initialize(name)  # Имеет название, которое указывается при ее создании
     self.name = name  # ВОПРОС: для присваивания значения instance variable принято использовать @name или метод-сеттер self.name= ?
-    self.trains_list = []
+    @trains = []
   end
 
-  def accept_train(train_name)
-    self.trains_list << train_name
+  def add_train(train)  # Может принимать поезда (по одному за раз)
+    @trains << train
+  end
+
+  def train_list  # Может возвращать список всех поездов на станции, находящиеся в текущий момент
+    @trains.map { |train| train.name }
+  end
+
+  def train_list_by_type(type)  # Может возвращать список поездов на станции по типу: кол-во грузовых, пассажирских
+    @trains.map { |train| train.name if train.type == type }.select { |train| train != nil }
+  end
+
+  def send_train(train)
+    @trains.delete(train)
   end
 end
 
@@ -16,17 +31,18 @@ end
 # ========== ПРОВЕРКА КЛАССА Station - НАЧАЛО ==========
 station1 = Station.new("Station1")
 puts "Наименование станции: #{station1.name}"
-station1.accept_train("train1")
-station1.accept_train("train2")
-station1.accept_train("train3")
-puts "Количество поездов на станции: #{station1.trains_list.length}"
-puts "Список поездов в текущий момент: #{station1.trains_list}"
-puts
 
-station2 = Station.new("Station2")
-puts "Наименование станции: #{station2.name}"
-station2.accept_train("train1")
-puts "Количество поездов на станции: #{station2.trains_list.length}"
-puts "Список поездов в текущий момент: #{station2.trains_list}"
-puts
+train1 = Train.new("train1", "passenger", 10)
+station1.add_train(train1)
+train2 = Train.new("train2", "cargo", 20)
+station1.add_train(train2)
+
+puts "Список всех поездов: #{station1.train_list}"
+puts "Список всех товарных поездов: #{station1.train_list_by_type("cargo")}"
+puts "Список всех пассажирских поездов: #{station1.train_list_by_type("passenger")}"
+
+puts "Список всех поездов: #{station1.train_list}"
+station1.send_train(train1)
+puts "Список всех поездов: #{station1.train_list}"
+
 # ========== ПРОВЕРКА КЛАССА Station - КОНЕЦ ==========
--- lesson3-train-station/Train.rb
+++ lesson3-train-station/Train.rb
@@ -1,10 +1,10 @@
 class Train
-  attr_accessor :train_id, :train_type, :train_wagon_count, :speed
+  attr_accessor :name, :type, :wagon_count, :speed
 
-  def initialize(train_id, train_type, train_wagon_count)
-    self.train_id = train_id
-    self.train_type = train_type
-    self.train_wagon_count = train_wagon_count
+  def initialize(name, type, wagon_count)
+    self.name = name
+    self.type = type
+    self.wagon_count = wagon_count
     self.speed = 0
   end
 
@@ -17,38 +17,10 @@ class Train
   end
 
   def add_wagon
-    self.train_wagon_count += 1 if self.speed == 0
+    self.wagon_count += 1 if self.speed == 0
   end
 
   def remove_wagon
-    self.train_wagon_count -= 1 if self.speed == 0
+    self.wagon_count -= 1 if self.speed == 0
   end
 end
-
-# ========== ПРОВЕРКА КЛАССА Train - НАЧАЛО ==========
-train1 = Train.new("train1", "passenger", 10)
-p train1
-puts "#{train1.train_id} это поезд типа #{train1.train_type} с #{train1.train_wagon_count} вагонами"
-puts "Скорость поезда - #{train1.speed}"
-puts "Количество вагонов - #{train1.train_wagon_count}"
-train1.add_wagon
-puts "Количество вагонов (попытка добавить вагон на скорости 0) - #{train1.train_wagon_count}"
-
-train1.accelerate(10)
-puts "Скорость поезда - #{train1.speed}"
-puts "Количество вагонов - #{train1.train_wagon_count}"
-train1.add_wagon
-puts "Количество вагонов (попытка добавить вагон на скорости) - #{train1.train_wagon_count}"
-
-train1.stop
-puts "Скорость поезда - #{train1.speed}"
-puts "Количество вагонов - #{train1.train_wagon_count}"
-train1.add_wagon
-puts "Количество вагонов (попытка добавить вагон после остановки) - #{train1.train_wagon_count}"
-puts
-
-train2 = Train.new("train2", "cargo", 20)
-p train2
-puts "#{train2.train_id} это поезд типа #{train2.train_type} с #{train2.train_wagon_count} вагонами"
-puts
-# ========== ПРОВЕРКА КЛАССА Train - КОНЕЦ ==========
